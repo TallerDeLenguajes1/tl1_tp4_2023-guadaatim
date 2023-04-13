d4d88cd503f3417a7c4d30084883e47c7688af0b
guadalupe atim <guadiiatim1309@gmail.com>
2023-04-13 19:11:15 -0300

    reservas de memoria

--- tp4_2.c
+++ tp4_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 struct Tarea{
     int tareaID; //numerado ciclo iterativo
@@ -13,23 +14,29 @@ int main(){
     int cantidad;
 
     printf("\ningrese cuantas tareas debe realizar: ");
-    scang("%d",&cantidad);
+    scanf("%d",&cantidad);
 
     tarea **tareaspendientes = (struct Tarea **) malloc(sizeof(struct Tarea *) * cantidad); //vector grande de punteros
     tarea **tareasrealizadas = (struct Tarea **) malloc(sizeof(struct Tarea*) * cantidad);
 
     for (int i = 0; i < cantidad; i++)
     {
-        tareaspendientes[i] = NULL;
-        tareaspendientes[i]->tareaID = i;
+        tareaspendientes[i] = NULL; //puntero a estructura
+    }
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        tareaspendientes[i] = (struct Tarea *)malloc(sizeof(struct Tarea) * cantidad); //reserve memoria para la estructura
+        tareaspendientes[i]->tareaID = i; 
+        //tareaspendientes[i]->descripcion = (char *) malloc(sizeof);
         printf("\ningrese una descripcion de la tarea: ");
-        scanf("%d",&tareaspendientes[i]->descripcion);
+        char buff[50];
+        gets(buff);
+        tareaspendientes[i]->descripcion = malloc(sizeof(char) * strlen(buff + 1));
+        strcpy(tareaspendientes[i]->descripcion, buff);
         printf("\ningrese la duracion de la tarea: ");
         scanf("%d",&tareaspendientes[i]->duracion);
     }
-    
-    
-
     
 
 
