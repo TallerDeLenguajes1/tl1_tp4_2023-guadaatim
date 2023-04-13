269fb52bbfbb65ca6423a6fb57a1afcdde38cc14
guadalupe atim <guadiiatim1309@gmail.com>
2023-04-13 19:46:13 -0300

    tareas realizadas

--- tp4_2.c
+++ tp4_2.c
@@ -22,25 +22,55 @@ int main(){
     for (int i = 0; i < cantidad; i++)
     {
         tareaspendientes[i] = NULL; //puntero a estructura
+        tareasrealizadas[i] = NULL;
     }
 
     for (int i = 0; i < cantidad; i++)
     {
         tareaspendientes[i] = (struct Tarea *)malloc(sizeof(struct Tarea) * cantidad); //reserve memoria para la estructura
         tareaspendientes[i]->tareaID = i; 
-        //tareaspendientes[i]->descripcion = (char *) malloc(sizeof);
+        fflush(stdin);
         printf("\ningrese una descripcion de la tarea: ");
         char buff[50];
         gets(buff);
         tareaspendientes[i]->descripcion = malloc(sizeof(char) * strlen(buff + 1));
         strcpy(tareaspendientes[i]->descripcion, buff);
+        fflush(stdin);
         printf("\ningrese la duracion de la tarea: ");
         scanf("%d",&tareaspendientes[i]->duracion);
     }
     
+    int realizada, j = 0;
 
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("\nTarea %d", tareaspendientes[i]->tareaID);
+        printf("\nDescripcion: %s", tareaspendientes[i]->descripcion);
+        printf("\nDuracion: %d", tareaspendientes[i]->duracion);
 
+        printf("\nSe realizo la tarea? 1-si, 0-no: ");
+        scanf("%d",&realizada);
 
+        if (realizada == 1)
+        {
+            tareasrealizadas[j] = tareaspendientes[i];
+            tareaspendientes[i] = NULL;
+            j++;
+        }
+        
+    }
+    
+    printf("\ntareas realizadas");
+    
+    int k = j;
+
+    for (int k = 0; k < j; k++)
+    {
+        printf("\nTarea %d", tareasrealizadas[k]->tareaID);
+        printf("\nDescripcion: %s", tareasrealizadas[k]->descripcion);
+        printf("\nDuracion: %d", tareasrealizadas[k]->duracion);
+    }
+    
     return 0;
 }
 
